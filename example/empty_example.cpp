1be1a174623471dd12cc599d5fd463e261a03138
Junekey Jeon <j6jeon@ucsd.edu>
2023-08-14 13:31:36 -0700

    Make it cleaner a little bit

--- example/empty_example.cpp
+++ example/empty_example.cpp
@@ -1,22 +1,52 @@
 #include "idiv/idiv.h"
+#include <format>
 #include <iostream>
 
-int main() {
-    std::uint64_t numerator = 7;
-    std::uint64_t denominator = 18;
-    std::uint64_t nmax = 0xffff'ffff;
+std::ostream& operator<<(std::ostream& out, jkj::big_uint::var const& n) {
+    auto n_copy = n;
+    auto const divisor = jkj::big_uint::var{UINT64_C(1000'0000'0000'0000'0000)};
+    std::vector<std::uint64_t> nineteen_digits;
+    while (!n_copy.is_zero()) {
+        auto q = n_copy.long_division(divisor);
+        if (n_copy.is_zero()) {
+            nineteen_digits.push_back(0);
+        }
+        else {
+            nineteen_digits.push_back(n_copy[0]);
+        }
+        n_copy = std::move(q);
+    }
 
-    auto info1 = jkj::idiv::convert_to_multiply_shift_effectively_rational({numerator, denominator},
-                                                                           nmax);
+    if (nineteen_digits.empty()) {
+        out << "0";
+    }
+    else {
+        auto itr = nineteen_digits.crbegin();
+        out << std::format("{}", *itr);
+
+        for (++itr; itr != nineteen_digits.crend(); ++itr) {
+            out << std::format("{:19}", *itr);
+        }
+    }
+
+    return out;
+}
+
+int main() {
+    jkj::big_uint::var numerator{7};
+    jkj::big_uint::var denominator{18};
+    jkj::big_uint::var nmax = 0xffff'ffff;
+
+    auto info1 =
+        jkj::idiv::convert_to_multiply_shift_effectively_rational({numerator, denominator}, nmax);
 
     auto info2 = jkj::idiv::convert_to_multiply_add_shift_effectively_rational(
         {numerator, denominator}, nmax,
         jkj::big_uint::var{0xffff'ffff'ffff'ffff, 0xffff'ffff'ffff'ffff});
 
     std::cout << "     Number = " << numerator << " / " << denominator
-              << "\n\n Multiplier = " << info1.multiplier[0]
+              << "\n\n Multiplier = " << info1.multiplier
               << "\n      Shift = " << info1.shift_amount
-              << "\n\n Multiplier = " << info2->multiplier[0]
-              << "\n      Adder = " << (info2->adder.is_zero() ? 0 : info2->adder[0])
+              << "\n\n Multiplier = " << info2->multiplier << "\n      Adder = " << info2->adder
               << "\n      Shift = " << info2->shift_amount << "\n";
 }
