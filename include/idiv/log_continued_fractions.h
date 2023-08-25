9fe0ed6e34b18c7a6788940330fbc3d322cf86c3
Junekey Jeon <j6jeon@ucsd.edu>
2023-08-25 02:26:36 -0700

    Add log_continued_fractions skeleton

---
+++ include/idiv/log_continued_fractions.h
@@ -0,0 +1,28 @@
+// Copyright 2023 Junekey Jeon
+//
+// The contents of this file may be used under the terms of
+// the Apache License v2.0 with LLVM Exceptions.
+//
+//    (See accompanying file LICENSE-Apache or copy at
+//     https://llvm.org/foundation/relicensing/LICENSE.txt)
+//
+// Alternatively, the contents of this file may be used under the terms of
+// the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE-Boost or copy at
+//     https://www.boost.org/LICENSE_1_0.txt)
+//
+// Unless required by applicable law or agreed to in writing, this software
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.
+
+#ifndef JKJ_HEADER_LOG_CONTINUED_FRACTIONS
+#define JKJ_HEADER_LOG_CONTINUED_FRACTIONS
+
+#include "continued_fractions.h"
+
+namespace jkj {
+    // Computes the continued fractions of ln(p/q) using Makino-Aoyama algorithm.
+
+}
+
+#endif
