49a904732f271aec5996c36a3a99de74f96c0fd4
Junekey Jeon <j6jeon@ucsd.edu>
2023-08-24 20:47:32 -0700

    Restructure the subproject

---
+++ subproject/test/source/unit_test.cpp
@@ -0,0 +1,7 @@
+#include "idiv/wuint.h"
+#include "idiv/bigint.h"
+#include "idiv/rational_continued_fractions.h"
+
+int main()
+{
+}
--- test/source/unit_test.cpp
+++
@@ -1,7 +0,0 @@
-#include "idiv/idiv.hpp"
-
-auto main() -> int
-{
-  auto result = name();
-  return result == "idiv" ? 0 : 1;
-}
