dba022ba39eb5ae49a1f6924df6aaae0b8f214d7
Junekey Jeon <j6jeon@ucsd.edu>
2023-09-10 15:50:11 -0700

    Adapt new continued fraction interface

--- include/idiv/best_rational_approx.h
+++ include/idiv/best_rational_approx.h
@@ -23,75 +23,122 @@
 #include <cstdlib>
 
 namespace jkj {
-    template <class ConvergentType>
-    struct best_rational_approx_output {
-        ConvergentType below;
-        ConvergentType above;
-    };
+    namespace idiv {
+        namespace detail {
+            // Common framework for two functions below.
+            template <class ReturnType, class ContinuedFractionGenerator, class UInt,
+                      class AfterTerminate>
+            constexpr ReturnType find_best_rational_approx_impl(ContinuedFractionGenerator& cf,
+                                                                UInt const& nmax,
+                                                                AfterTerminate&& after_terminate) {
+                util::constexpr_assert(util::is_strictly_positive(nmax));
 
-    // Find the best rational approximations from below and from above of denominators no more than
-    // denominator_upper_bound for the given number x. The number x is given in terms of its
-    // continued fractions. The continued fractions calculator cf is assumed to be initialized,
-    // i.e., it starts from the first convergent when evaluated.
-    template <class ConvergentGenerator, class UInt>
-    constexpr best_rational_approx_output<typename ConvergentGenerator::convergent_type>
-    find_best_rational_approx(ConvergentGenerator& cf, UInt const& denominator_upper_bound) {
-        util::constexpr_assert(is_strictly_positive(denominator_upper_bound));
+                using convergent_type = typename ContinuedFractionGenerator::convergent_type;
 
-        using convergent_type = typename ConvergentGenerator::convergent_type;
-        best_rational_approx_output<convergent_type> ret_value;
+                // First, find the last convergent and the last semiconvergent whose denominator is
+                // bounded above by nmax.
 
-        // First, find the last convergent whose denominator is bounded above by the given upper
-        // bound.
-        convergent_type previous_convergent;
-        convergent_type current_convergent;
-        do {
-            previous_convergent = cf.previous_convergent();
-            current_convergent = cf.current_convergent();
+                auto get_last_semiconvergent = [&]() {
+                    auto semiconvergent_coeff =
+                        (nmax - cf.previous_previous_convergent_denominator()) /
+                        cf.previous_convergent_denominator();
 
-            if (cf.is_terminated()) {
-                // If there is no more convergents, we already obtained the perfect approximation.
-                ret_value.below = cf.current_convergent();
-                ret_value.above = cf.current_convergent();
-                return ret_value;
+                    return convergent_type{
+                        cf.previous_previous_convergent_numerator() +
+                            semiconvergent_coeff * cf.previous_convergent_numerator(),
+                        util::abs(cf.previous_previous_convergent_denominator() +
+                                  semiconvergent_coeff * cf.previous_convergent_denominator())};
+                };
+
+                while (cf.current_convergent_denominator() <= nmax) {
+                    if (cf.terminated()) {
+                        return after_terminate(get_last_semiconvergent);
+                    }
+
+                    cf.update();
+                }
+
+                // If there the last convergent is still not a perfect approximation, then we return
+                // the last semiconvergent and the convergent as the bounds. Which one is the lower
+                // bound and which one is the upper bound is determined by the parity of
+                // cf.current_index(). Note that cf.current_index() is the index of the first
+                // convergent with the denominator strictly larger than nmax, so if this index is
+                // even, then the semiconvergent is the lower bound and the convergent is the upepr
+                // bound, and if the index is odd, then the other way around.
+                if (cf.current_index() % 2 == 0) {
+                    return {get_last_semiconvergent(), cf.previous_convergent()};
+                }
+                else {
+                    return {cf.previous_convergent(), get_last_semiconvergent()};
+                }
             }
+        }
 
-            // Obtain the next convergent.
-            cf.update();
-        } while (cf.current_convergent_denominator() <= denominator_upper_bound);
-
-        // If the current convergent is of even index,
-        // then the current convergent is the best approximation from below,
-        // and the best approximation from above is the last semiconvergent.
-        // If the current convergent is of odd index, then the other way around.
-        // Note that cf.current_index() is one larger than the index of the current convergent,
-        // so we need to reverse the parity.
-
-        auto compute_bounds = [&](auto& major, auto& minor) {
-            // The current convergent is the best approximation from below.
-            major = current_convergent;
-
-            // The best approximation from above is the last semiconvergent.
-            using std::div;
-            auto semiconvergent_coeff =
-                div(denominator_upper_bound - previous_convergent.denominator,
-                    current_convergent.denominator)
-                    .quot;
-
-            minor.numerator =
-                previous_convergent.numerator + semiconvergent_coeff * current_convergent.numerator;
-            minor.denominator = previous_convergent.denominator +
-                                semiconvergent_coeff * current_convergent.denominator;
+        template <class ConvergentType>
+        struct best_rational_approx_output {
+            ConvergentType below;
+            ConvergentType above;
         };
 
-        if (cf.current_index() % 2 == 1) {
-            compute_bounds(ret_value.below, ret_value.above);
-        }
-        else {
-            compute_bounds(ret_value.above, ret_value.below);
+        // For a given real number x and a positive integer nmax, find the best rational
+        // approximation of it from below and from above whose denominators are no more than nmax.
+        // The number x is specified in terms of a continued fraction generator giving its continued
+        // fraction expansion. The generator needs to have index_tracker and
+        // previous_previous_convergent_tracker within it, and it also needs to be at its initial
+        // stage, i.e., the call to current_index() without calling update() should return -1.
+        template <class ContinuedFractionGenerator, class UInt>
+        constexpr best_rational_approx_output<typename ContinuedFractionGenerator::convergent_type>
+        find_best_rational_approx(ContinuedFractionGenerator& cf, UInt const& nmax) {
+            using convergent_type = typename ContinuedFractionGenerator::convergent_type;
+            using return_type = best_rational_approx_output<convergent_type>;
+
+            return detail::find_best_rational_approx_impl<return_type>(
+                cf, nmax, [&](auto&&) -> return_type {
+                    return {cf.current_convergent(), cf.current_convergent()};
+                });
         }
 
-        return ret_value;
+        // For a given real number x and a positive integer nmax, find the interval
+        // [max_n floor(nx)/n, min_n (floor(nx)+1)/n), where n ranges from {1, ... , nmax}. The
+        // number x is specified in terms of a continued fraction generator giving its continued
+        // fraction expansion. The generator needs to have index_tracker and
+        // previous_previous_convergent_tracker within it, and it also needs to be at its initial
+        // stage, i.e., the call to current_index() without calling update() should return -1.
+        template <class ContinuedFractionGenerator, class UInt>
+        constexpr interval<typename ContinuedFractionGenerator::convergent_type,
+                           interval_type_t::bounded_left_closed_right_open>
+        find_floor_quotient_range(ContinuedFractionGenerator& cf, UInt const& nmax) {
+            using convergent_type = typename ContinuedFractionGenerator::convergent_type;
+            using return_type =
+                interval<convergent_type, interval_type_t::bounded_left_closed_right_open>;
+
+            return detail::find_best_rational_approx_impl<return_type>(
+                cf, nmax, [&](auto&& get_last_semiconvergent) -> return_type {
+                    // If we reach to the perfect approximation, then we have to find the largest
+                    // positive integer v <= nmax such that vp == -1 (mod q), where x = p/q. Then
+                    // the lower bound is p/q, while the upper bound is ((vp+1)/q) / v.
+                    // To compute v, we find the modular inverse b of -p. This can be done by
+                    // observing that the best rational approximation from above whose denominator
+                    // is strictly less than q must be precisely ((bp+1)/q) / b. Then
+                    //        v = b + floor((nmax - b)/q)q and
+                    // (vp+1)/q = (bp+1)/q + floor((nmax - b)/q)p.
+
+                    // If we ended at an even convergent, the last convergent is the best rational
+                    // approximation from above. Otherwise, the last semiconvergent is the best
+                    // rational approximation from above.
+                    auto upper_bound = cf.current_index() % 2 == 0 ? cf.previous_convergent()
+                                                                   : get_last_semiconvergent();
+
+                    // At this point, upper_bound is ((bp+1)/q) / b, so we adjust the numerator and
+                    // the denominator by floor((nmax - b)/q).
+                    auto max_quotient =
+                        (nmax - upper_bound.denominator) / cf.current_convergent().denominator;
+                    upper_bound.numerator += max_quotient * cf.current_convergent().numerator;
+                    upper_bound.denominator += max_quotient * cf.current_convergent().denominator;
+
+                    return {cf.current_convergent(), static_cast<convergent_type&&>(upper_bound)};
+                });
+        }
     }
 }
 
