f09e2afedeadd0b2ac5860d2497262a7178db507
Junekey Jeon <j6jeon@ucsd.edu>
2024-07-04 15:33:59 +0900

    Complete the example

--- subproject/example/xi_zeta_region_json.cpp
+++ subproject/example/xi_zeta_region_json.cpp
@@ -163,9 +163,6 @@ struct daw::json::json_data_contract<continued_fraction_generator> {
     struct constructor_t {
         continued_fraction_generator operator()(std::string_view type,
                                                 std::string_view params_raw_json) const {
-            using impl_type = ::jkj::cntfrc::impl::type_erased<partial_fraction_type,
-                                                               convergent_type, interval_type>;
-
             if (type == "rational") {
                 return continued_fraction_generator{from_json<
                     ::jkj::cntfrc::impl::rational<::jkj::bigint::int_var, ::jkj::bigint::uint_var>>(
@@ -193,4 +190,120 @@ struct daw::json::json_data_contract<continued_fraction_generator> {
     };
 };
 
-int main() {}
+template <>
+struct daw::json::json_data_contract<
+    jkj::interval<jkj::bigint::int_var, jkj::interval_type_t::bounded_closed>> {
+    using type = json_member_list<json_number<"min", std::int_least64_t>,
+                                  json_number<"max", std::int_least64_t>>;
+};
+
+struct input_params {
+    continued_fraction_generator x;
+    continued_fraction_generator y;
+    std::vector<jkj::interval<jkj::bigint::int_var, jkj::interval_type_t::bounded_closed>> ranges;
+};
+
+template <>
+struct daw::json::json_data_contract<input_params> {
+    using type = json_member_list<
+        json_class<"x", continued_fraction_generator>,
+        json_class<"y", continued_fraction_generator>,
+        json_array<"ranges", ::jkj::interval<::jkj::bigint::int_var,
+                                             ::jkj::interval_type_t::bounded_closed>>>;
+};
+
+#include <fstream>
+#include <sstream>
+
+namespace jkj {
+    namespace bigint {
+        static std::ostream& operator<<(std::ostream& out, jkj::bigint::uint_var const& n) {
+            auto digits = n.to_decimal();
+            if (digits.empty()) {
+                out << "0";
+            }
+            else {
+                auto itr = digits.cbegin();
+                out << std::format("{}", *itr);
+
+                for (++itr; itr != digits.cend(); ++itr) {
+                    out << std::format("{:019d}", *itr);
+                }
+            }
+
+            return out;
+        }
+
+        static std::ostream& operator<<(std::ostream& out, jkj::bigint::int_var const& n) {
+            if (n.is_strictly_negative()) {
+                out << "-";
+            }
+            out << n.abs();
+            return out;
+        }
+    }
+}
+
+int main() {
+    auto const params = [] {
+        std::ifstream input_file{"xi_zeta_region_json_example_input.json"};
+        std::stringstream strstream;
+        strstream << input_file.rdbuf();
+        return daw::json::from_json<input_params>(strstream.str());
+    }();
+
+    auto const result = jkj::idiv::find_xi_zeta_region(params.x, params.y, params.ranges);
+
+    for (auto const& elementary_region : result) {
+        if (jkj::util::is_zero(elementary_region.xi_left_endpoint_denominator)) {
+            elementary_region.zeta_range.visit([](auto const& itv) {
+                using itv_type = std::remove_cvref_t<decltype(itv)>;
+
+                if constexpr (itv_type::interval_type() == jkj::interval_type_t::bounded_open) {
+                    std::cout << itv.lower_bound().numerator << " / "
+                              << itv.lower_bound().denominator << " <= zeta <"
+                              << itv.upper_bound().numerator << " / "
+                              << itv.upper_bound().denominator << ")\n\n";
+                }
+            });
+            break;
+        }
+
+        std::cout << "When zeta ";
+        elementary_region.zeta_range.visit([](auto const& itv) {
+            using itv_type = std::remove_cvref_t<decltype(itv)>;
+
+            if constexpr (itv_type::interval_type() == jkj::interval_type_t::bounded_open) {
+                std::cout << "in (" << itv.lower_bound().numerator << " / "
+                          << itv.lower_bound().denominator << ", " << itv.upper_bound().numerator
+                          << " / " << itv.upper_bound().denominator << "):\n";
+            }
+            else if constexpr (itv_type::interval_type() == jkj::interval_type_t::bounded_closed) {
+                std::cout << "= " << itv.lower_bound().numerator << " / "
+                          << itv.lower_bound().denominator << ":\n";
+            }
+            else {
+                std::cout << "in (-inf, inf):\n";
+            }
+        });
+
+        std::cout << "  (" << elementary_region.xi_left_endpoint_numerator << " - zeta) / "
+                  << elementary_region.xi_left_endpoint_denominator;
+
+        if (elementary_region.xi_left_endpoint_included) {
+            std::cout << " <= xi ";
+        }
+        else {
+            std::cout << " < xi ";
+        }
+        if (elementary_region.xi_right_endpoint_included) {
+            std::cout << "<= ";
+        }
+        else {
+            std::cout << "< ";
+        }
+
+        std::cout << "(" << elementary_region.xi_right_endpoint_numerator << " - zeta) / "
+                  << elementary_region.xi_right_endpoint_denominator << "\n\n";
+    }
+}
